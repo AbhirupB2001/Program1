c6719e6aaabd32abcacf435aa83ee584721e5b60
AbhirupB2001 <49182204+AbhirupB2001@users.noreply.github.com>
2019-04-02 11:59:15 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,19 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+ 
+int main()
 {
-printf("Hello World");
+  int row, c, n, s;
+  printf("Enter the number of rows in pyramid of stars you wish to see\n");
+  scanf("%d", &n);
+  s = n;
+  for (row = 1; row <= n; row++)  
+  {
+    for (c = 1; c < s; c++)  
+      printf(" ");
+    s--;
+    for (c = 1; c <= 2*row - 1; c++) 
+      printf("*");
+    printf("\n");
+  }
+  return 0;
 }
